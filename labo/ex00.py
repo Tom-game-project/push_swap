16e925cb1dd9319135cafa63777e0768335e16b6
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2024-09-09 01:57:07 +0900

    add `swap_stack_b_1` function

--- labo/ex00.py
+++ labo/ex00.py
@@ -69,7 +69,9 @@ class push_swap:
 
 
 
-def swap_stack_b(ps:push_swap,index_a, index_b):
+def swap_stack_b_0(ps:push_swap,index_a, index_b):
+    # step index_b * 2 + 4
+    print("step predict", index_b * 2 + 4)
     for i in range(index_a + 1):
         ps.run(instruction.pa)
     for i in range(index_b - index_a):
@@ -81,6 +83,26 @@ def swap_stack_b(ps:push_swap,index_a, index_b):
         ps.run(instruction.rrb)
     for i in range(index_a + 1):
         ps.run(instruction.pb)
+
+def swap_stack_b_1(ps:push_swap, index_a, index_b, stack_b_length):
+    print("step predict",4 * stack_b_length + 2 - 2 * (index_a + index_b))
+    a = stack_b_length - index_a - 1
+    b = stack_b_length - index_b - 1
+    # print("step predict", 2 * (a + b) + 6)
+    for i in range(b + 1): # 2*(b+1)
+        ps.run(instruction.rrb)
+        ps.run(instruction.pa)
+    for i in range(a - b): 
+        ps.run(instruction.rrb)
+    ps.run(instruction.pb)
+    ps.run(instruction.rb)
+    ps.run(instruction.pa)
+    for i in range(a - b - 1):
+        ps.run(instruction.rb)
+    for i in range(b + 1):
+        ps.run(instruction.pb)
+        ps.run(instruction.rb)
+
 
 def test00():
     psw = push_swap([2,1,3,6,5,8])
@@ -101,11 +123,22 @@ def test00():
 
 def test01():
     psw = push_swap([])
-    psw.stack_b = [0,1,2,3,4,5,6,7,8,9]
-    swap_stack_b(psw, 2, 6)
+    psw.stack_b = [i for i in range(100)]
+    swap_stack_b_0(psw, 97, 99)
     print(list(range(10)))
     print(psw.stack_b)
+    print("real step", psw.step)
+
+def test02():
+    psw = push_swap([])
+    psw.stack_b = [i for i in range(100)]
+    swap_stack_b_1(psw, 97, 99, len(psw.stack_b))
+    print(list(range(10)))
+    print(psw.stack_b)
+    print("real step", psw.step)
+
 
 if __name__ == "__main__":
     # test00()
-    test01()
+    # test01()
+    test02()
