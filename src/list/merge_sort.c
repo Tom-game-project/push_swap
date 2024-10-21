38e1db40ae88db72624ea5d0d7899c0b0e45558e
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2024-10-21 12:35:52 +0900

    [WIP] optimize and add operation

--- src/list/merge_sort.c
+++ src/list/merge_sort.c
@@ -1,29 +1,97 @@
 #include <stdlib.h>
 #include "list.h"
 
+#include "../../src/output/output.h"
+
+
+/**
+ * 最適化ルール
+ * pa pb -> nop
+ * pb pa -> nop
+ * rra rrb -> rrr
+ * rrb rra -> rrr
+ */
+#define SA 0
+#define SB 1
+#define SS 2
+
+#define PA 3
+#define PB 4
+
+#define RA 5
+#define RB 6
+#define RR 7
+
+#define RRA 8
+#define RRB 9
+#define RRR 10
+
+
+static void push_end(t_node **f, t_node**t, char f_f)
+{
+    insert(t, 0, pop(f, len(*f) - 1));
+    if (f_f == 'a')
+    {
+        put_str("rra\n");
+        put_str("pb\n");
+    }
+    else
+    {
+        put_str("rrb\n");
+        put_str("pa\n");
+    }
+}
+
+static void push_head(t_node **f, t_node**t, char f_f)
+{
+    insert(t, 0, pop(f, 0));
+    if (f_f == 'a')
+        put_str("pb\n");
+    else
+        put_str("pa\n");
+}
+
+static void push_second(t_node **f, t_node**t, char f_f)
+{
+    insert(t, 0, pop(f, 1));
+    if (f_f == 'a')
+    {
+        put_str("sa\n");
+        put_str("pb\n");
+    }
+    else
+    {
+        put_str("sb\n");
+        put_str("pa\n");
+    }
+}
 
 /// @brief 
 /// @param f from
 /// @param t to
-static void move_stack(t_node **f, t_node **t)
+static void move_stack(t_node **f, t_node **t,char f_f)
 {
     int i;
 
     i = 1;
     if (get_elem(*f, 0) * i < get_elem(*f, len(*f) - 1) * i)
-        insert(t, 0, pop(f, len(*f) - 1));
+        push_end(f, t, f_f);
     else
-        insert(t, 0, pop(f, 0));
+        push_head(f, t, f_f);
     while (*f != NULL){
         if (get_elem(*f, 0) * i < get_elem(*t, 0) * i)
         {
             if (get_elem(*f, 0) * i < get_elem(*f, len(*f) - 1) * i && \
                 get_elem(*f, len(*f) - 1) * i < get_elem(*t, 0) * i)
-                insert(t, 0, pop(f, len(*f) - 1));
+                push_end(f, t, f_f);
             else
-                insert(t, 0, pop(f, 0));
+                push_head(f, t, f_f);
         }
-        else 
+        else if (get_elem(*f, len(*f) - 1) * i < get_elem(*t, 0) * i)
+            push_end(f, t,f_f);
+        else if (1 < len(*f) && get_elem(*f, 1) * i < get_elem(*t, 0) * i)
+            push_second(f, t,f_f);
+        else
             i *= -1;
     }
 }
@@ -32,7 +100,7 @@ void merge_sort(t_node **node_a,t_node **node_b)
 {
     while (/*not*/ !is_sorted (*node_a))
     {
-        move_stack(node_a, node_b); // from node_a to node_b
-        move_stack(node_b, node_a); // from node_b to node_a
+        move_stack(node_a, node_b, 'a'); // from node_a to node_b
+        move_stack(node_b, node_a, 'b'); // from node_b to node_a
     }
 }
