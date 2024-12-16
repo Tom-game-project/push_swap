d5cccb6423f7d9bbbbdec704057799d6ea669c2b
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2024-12-16 15:55:39 +0900

    add `check_deuplicate` fnuction

--- src/push_swap.c
+++ src/push_swap.c
@@ -53,29 +53,52 @@ int set_stack_a(t_node **a,int argc, char *argv[])
 {
     int *stack;
     int i;
-    int error_flag;
 
     i = 0;
-     error_flag = 0;   
     stack = create_list(argc, argv);
     if (stack == NULL)
-	    return (1);
+	    return (print_allocation_error(), 1);
     while (++i < argc)
-	    error_flag = push(a,stack[i - 1]);
+    {
+	     if (push(a,stack[i - 1]))
+		     return (print_allocation_error(), free(stack), 1);
+    }
     free(stack);
-    return (error_flag);
+    return (0);
+}
+
+/// 重複を検知します
+/// 重複を検知すると1を返す
+int check_duplicate(t_node **node_a)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < len(*node_a))
+	{
+		j = 0;
+		while (j < len(*node_a))
+		{
+			if (i != j && get_elem(*node_a, i) == get_elem(*node_a, j))
+				return (1);
+			j += 1;
+		}
+		i += 1;
+	}
+	return (0);
 }
 
 int my_sort(int (*func[5])(t_node **node_a,t_node **node_b, t_node **ops), t_node **node_a,t_node **node_b, t_node **ops)
 {
 	int a_len;
 
+	a_len = len(*node_a);
 	func[0] = len2_sort;
 	func[1] = len3_sort;
 	func[2] = len4_sort;
 	func[3] = len5_sort;
 	func[4] = merge_sort;
-	a_len = len(*node_a);
 	if (a_len == 1)
 		return (0);
 	else if (2 <= a_len && a_len <= 5)
@@ -98,9 +121,11 @@ int main(int argc, char *argv[])
     ops = NULL;
     if (set_stack_a(&a, argc, argv))
         return print_error();
+    if (check_duplicate(&a))
+	    return (print_error(), clear(&a), 1);
     if (my_sort(func, &a, &b, &ops))
 	    return (print_allocation_error(), clear(&a), clear(&a), clear(&b), 1);
     else
-	    return (output_all_ops(&ops), clear(&a), clear(&b), 0) ;
+	    return (output_all_ops(&ops), clear(&a), clear(&b), 0);
     return (0);
 }
